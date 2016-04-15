bc0146a727985c7e435d721d63d255a979c885fe
Dan Flettre <fletd01@yahoo.com>
2016-04-15 18:46:38 -0500

    switch to using phaser from npm per comment: https://github.com/photonstorm/phaser/issues/1974#issuecomment-186923141

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import 'pixi'
+import 'p2'
 import Phaser from 'phaser'
 
 import BootState from './states/Boot'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 var path = require('path')
 var webpack = require('webpack')
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/')
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+var pixi = path.join(phaserModule, 'build/custom/pixi.js')
+var p2 = path.join(phaserModule, 'build/custom/p2.js')
+
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'))
 })
@@ -29,22 +36,24 @@ module.exports = {
       server: {
         baseDir: ['./', './build']
       }
-    }),
-
+    })
   ],
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
-      }
+      { test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'src') },
+      { test: /pixi\.js/, loader: 'expose?PIXI' },
+      { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
+      { test: /p2\.js/, loader: 'expose?p2' }
     ]
   },
   node: {
     fs: 'empty'
   },
-  externals: {
-    'phaser': 'Phaser'
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2
+    }
   }
 }
