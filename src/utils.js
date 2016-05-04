04fd323c8e93fab3bb93e301c1fb5e4812071d1c
lean <leaocabrera@gmail.com>
2016-05-04 10:57:51 -0300

    remove generated bundles

--- src/utils.js
+++ src/utils.js
@@ -4,14 +4,6 @@ export const centerGameObjects = (objects) => {
   })
 }
 
-export const getRandomInt = (min, max) => {
-  if (max == null) {
-    max = min
-    min = 0
-  }
-  return min + Math.floor(Math.random() * (max - min + 1))
-}
-
 export const setResponsiveWidth = (sprite, percent, parent) => {
   let percentWidth = (sprite.texture.width - (parent.width / (100 / percent))) * 100 / sprite.texture.width
   sprite.width = parent.width / (100 / percent)
