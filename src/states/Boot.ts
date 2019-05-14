e16b7e23722776f02f4e80488d94e2e66fcb97e5
thechutrain <chu.chuyalan@gmail.com>
2019-05-14 16:16:27 -0400

    converted boot js -> ts

--- src/states/Boot.ts
+++ src/states/Boot.ts
@@ -1,7 +1,9 @@
-import Phaser from 'phaser'
-import WebFont from 'webfontloader'
+import * as Phaser from 'phaser-ce'
+import * as WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
+  fontsReady: boolean;
+  
   init () {
     this.stage.backgroundColor = '#EDEEC9'
     this.fontsReady = false
