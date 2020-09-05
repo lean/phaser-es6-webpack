b766ee3cac1b28506952cbc98b8a0c6f7ab67686
Daniil Polovinkin <dolovinkin@gmail.com>
2020-09-05 15:59:14 +0300

    Feature(choice-wheel): Добавлено колесо выбора варианта действия

---
+++ src/UI/ChoiceWheelHelper.js
@@ -0,0 +1,36 @@
+import { ChoiceWheel as ChoiceWheelCmp } from "../components/ChoiceWheel";
+
+class ChoiceWheelHelper {
+  constructor() {
+    this.wheelVisible = false
+  }
+
+  _closeWheel() {
+    this.wheel.destroy()
+    this.wheel = undefined
+    this.wheelVisible = false
+  }
+
+  setGame(game) {
+    this.game = game
+  }
+
+  openAt(x, y, options) {
+    return new Promise((resolve) => {
+      if(this.wheelVisible) {
+        this._closeWheel()
+      }
+      const wheel = new ChoiceWheelCmp(this.game, x, y, options)
+      wheel.onSelect.add(selectedOption => {
+        resolve(selectedOption)
+        this._closeWheel()
+      })
+      this.wheel = wheel
+      this.wheelVisible = true
+    })
+  }
+}
+
+const choiceWheel = new ChoiceWheelHelper()
+
+export default choiceWheel
