93e78f0cd9f76e874bf350829f46ddb205dee3ec
alexandra.l <aalexandralobovikova@gmail.com>
2020-09-06 13:14:48 +0300

    f

---
+++ src/states/phone/contactsRingingScreen.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser'
+import { centerGameObjects } from '../../utils'
+import BackButton from '../../components/BackButton'
+
+export default class extends Phaser.State {
+  init () { }
+  preload () {
+  }
+
+  create () {
+    this.contactsBg = this.add.sprite(0, 0, 'contactsRinging')
+    BackButton.addButton(this.game, this.state, 'MainPhoneScreen', 'back')
+    BackButton.addButton(this.game, this.state, 'MainPhoneScreen', 'close')
+  }
+}
